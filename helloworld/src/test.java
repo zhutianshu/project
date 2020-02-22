9419216a5b6de10b34402f092cbe84e382b7ef4d
zhutianshu <zxc8986944@gmail.com>
2020-02-22 21:06:43 +0800

    num在零到十之间

---
+++ helloworld/src/test.java
@@ -0,0 +1,20 @@
+public class test {
+
+    public static void main(String[] args) {
+
+        int num = 50;
+
+        if (num > 0 && num < 10) {
+            System.out.println("num在零到十之间");
+        } else if (10 < num && num < 20) {
+            System.out.println("num在十到二十之间");
+        } else {
+            System.out.println("条件不再成立");
+        }
+    }
+
+
+
+
+
+}
